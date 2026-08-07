1f1cc71abf8ecefae3c2491d7e5a71b8c3a732c9
ChiragMR27 <chiragmr676@gmail.com>
2026-08-07 15:51:14 +0530

    Added global CORS configuration to allow preflight option requests

---
+++ src/main/java/com/lifesync/family_service/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.lifesync.family_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**") // Apply to all endpoints
+                        .allowedOrigins("*") // Allow all frontend URLs
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Explicitly allow OPTIONS!
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
